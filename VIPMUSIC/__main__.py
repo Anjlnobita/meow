032ba8fd2b071ea07abbe0f946e7674e09e24dda
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-11 18:12:48 +0530

    Update __main__.py

--- VIPMUSIC/__main__.py
+++ VIPMUSIC/__main__.py
@@ -1,5 +1,6 @@
 import asyncio
 import importlib
+from telegram.ext import Application
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
