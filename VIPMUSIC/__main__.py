77a6103744f599ea7edbb94e6eb833a40aefd976
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-11 17:23:02 +0530

    Update __main__.py

--- VIPMUSIC/__main__.py
+++ VIPMUSIC/__main__.py
@@ -1,23 +1,20 @@
-#
-# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
-#
-# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
 import asyncio
 import importlib
-
 from pyrogram import idle
-
 import config
 from config import BANNED_USERS
 from VIPMUSIC import HELPABLE, LOGGER, app, userbot
 from VIPMUSIC.core.call import VIP
 from VIPMUSIC.plugins import ALL_MODULES
 from VIPMUSIC.utils.database import get_banned_users, get_gbanned
+from telethon import TelegramClient
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
+# Telethon Client
+telethn = TelegramClient("hinata", api_id=config.API_ID, api_hash=config.API_HASH)
+
+# Application Builder
+app_builder = ApplicationBuilder().token(config.BOT_TOKEN)
 
 async def init():
     if (
@@ -31,6 +28,7 @@ async def init():
             "No Assistant Clients Vars Defined!.. Exiting Process."
         )
         return
+
     if not config.SPOTIFY_CLIENT_ID and not config.SPOTIFY_CLIENT_SECRET:
         LOGGER("VIPMUSIC").warning(
             "No Spotify Vars defined. Your bot won't be able to play spotify queries."
@@ -47,21 +45,25 @@ async def init():
         pass
 
     await app.start()
-
     for all_module in ALL_MODULES:
         imported_module = importlib.import_module(all_module)
-
         if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
             if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
                 HELPABLE[imported_module.__MODULE__.lower()] = imported_module
-    LOGGER("VIPMUSIC.plugins").info("Successfully Imported All Modules ")
-
+            LOGGER("VIPMUSIC.plugins").info("Successfully Imported All Modules ")
     await userbot.start()
     await VIP.start()
     await VIP.decorators()
+
+    # Telethon Client Start
+    await telethn.start()
+
+    # Application Builder Start
+    hina = app_builder.build()
+    await hina.start()
+
     LOGGER("VIPMUSIC").info("VIPMUSIC STARTED SUCCESSFULLY 🕊️")
     await idle()
-
 
 if __name__ == "__main__":
     asyncio.get_event_loop_policy().get_event_loop().run_until_complete(init())
